f8bc9a740f6f65f95f07f799400b211c416f22cd
Jason Hoffman <jasonandrewhoffman@gmail.com>
2014-08-04 06:39:47 -0500

    Update quickRoute.js
    Remove key

--- quickRoute.js
+++ quickRoute.js
@@ -2,9 +2,8 @@ var http = require('http');
 var rest = require('restler');
 
 var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
-var username = 'jasonandrewhoffman';
-var apiKey = '323da49e858c3d25fa589d1f6de62a96807f9dab';
-
+var username = 'user';
+var apiKey = 'key';
 var origin = 'KIAD';
 var destination = 'KORD';
 
